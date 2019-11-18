611ee0c9506049fb375f1ec1e4515f58bf8750a3
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-11-18 18:43:49 +0100

    Update clase1.java

--- boletin9/pkg1/clase1.java
+++ boletin9/pkg1/clase1.java
@@ -16,22 +16,29 @@ public class clase1 {
         return num;
     }
     public void verDatos(){
+        int contCeros=0,contPos=0,contNeg=0;
         do{
         num=pedirDatos();
         
         if(num>0){
             System.out.println("El numero es positivo");
+            contPos++;
         }
         if(num==0){
             System.out.println("El numero es cero");
+            contCeros++;
         }
         if(num<0){
             System.out.println("El b¡numero es negativo");
+            contNeg++;
         }
         c++;
         System.out.println(num);
-        }while(c<9);
+        }while(c<10);
         System.out.println("ACABO");
+        System.out.println("Numero de ceros: "+contCeros
+                        +"\nNumeros positivos: "+contPos
+                        +"\nNumeros negativos: "+contNeg);
 }
 }
 
