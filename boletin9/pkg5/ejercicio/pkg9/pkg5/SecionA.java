1e55cf1c3c021d373c1649102af7fd3d17781475
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-11-19 08:48:32 +0100

    Add files via upload

---
+++ boletin9/pkg5/ejercicio/pkg9/pkg5/SecionA.java
@@ -0,0 +1,23 @@
+
+
+package ejercicio.pkg9.pkg5;
+import java.util.Scanner;
+
+
+public class SecionA {
+   private int num;
+   Scanner sc = new Scanner(System.in); 
+   public void pedirInt(){
+    System.out.println("Introduce el numero");
+    num=sc.nextInt();
+    }
+    public void sucesionA(){
+    pedirInt();
+    int cont;int acu1=0;
+    for(cont=0;cont<num;cont++){
+    acu1=acu1+2;
+    System.out.println(acu1);
+    }
+    System.out.println("Fin de la cita a");
+    }
+}
---
+++ boletin9/pkg5/ejercicio/pkg9/pkg5/SecionB.java
@@ -0,0 +1,27 @@
+
+package ejercicio.pkg9.pkg5;
+import java.util.Scanner;
+
+
+public class SecionB {
+   private int num;
+   Scanner sc = new Scanner(System.in); 
+   public void pedirInt(){
+    System.out.println("Introduce el numero");
+    num=sc.nextInt();
+    }
+    public void sucesionB(){
+        pedirInt();
+        int cont;
+        int acul=0;
+
+    for (cont=0;cont<num;cont++){
+        if(acul>0)
+        acul=(acul+1)*(-1);
+        else{
+        acul=(acul-1)*(-1);}    
+        System.out.println(acul);
+        System.out.println("Fin serie b");
+    }
+}
+}
