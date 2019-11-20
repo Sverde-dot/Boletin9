7a7c3de02335a38a2d161eb979c971739e02cb41
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-11-20 08:44:55 +0100

    Update clase6.java

--- boletin9/pkg6/clase6.java
+++ boletin9/pkg6/clase6.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class clase6 {
  private int num;
  private int cont=0;
- private int Trab=0;
+ private int Trab1=0;
  private float Trab2=0;
 Scanner sc = new Scanner(System.in);
 
@@ -17,14 +17,14 @@ public void calcular(){
 do{ 
 pedirInt();
 if(num>=1000 && num<=1750)
-Trab++;    
+Trab1++;    
 else if (num<1000 && num>0)
 Trab2++;
 if (num!=0)
 cont++;
 }while(num!=0);
 if (cont!=0){
-System.out.println("Los empleados con un sueldo entre 1000 y 1750 y : "+Trab
+System.out.println("Los empleados con un sueldo entre 1000 y 1750 y : "+Trab1
 +"\nLa porcentaje de empleados con sueldo inferior a 1000es: "+(Trab2/cont)*100);
 }
 System.out.println("Fin de la cita");
